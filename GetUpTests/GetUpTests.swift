26bc9628a4238eb51234290d7928a5243776926a
gjsingh <gjsingh@gmail.com>
2026-02-24 13:13:06 -0800

    Initial Commit

---
+++ GetUpTests/GetUpTests.swift
@@ -0,0 +1,16 @@
+//
+//  GetUpTests.swift
+//  GetUpTests
+//
+//  Created by Gourav J Singh khurana on 2/24/26.
+//
+
+import Testing
+
+struct GetUpTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
